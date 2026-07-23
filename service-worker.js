e8675d651c1e125c75f3028328529f142bc38cb2
sd <stephendevinehp96@gmail.com>
2026-07-23 23:25:43 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,1 +0,0 @@
-const C="pitbull-prague-v1",A=["./","index.html","manifest.webmanifest","assets/css/styles.css","assets/js/app.js","assets/icons/icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
